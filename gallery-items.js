9222234338aefbda71ba8e9e0051511bc178ede6
Volodymyr Molodynia <molodunja91@gmail.com>
2020-09-13 01:45:27 +0300

    first commit

---
+++ gallery-items.js
@@ -0,0 +1,65 @@
+export default [
+  {
+    preview:
+    'https://lh3.googleusercontent.com/proxy/G1O4__1HZgB5Ox4TKHJvcTJUU2jjDEoPGf8M4iUpTFISfXmExiSB_ZDh6zf6z4-OPDYMiZvcmDDhi4EY9XdYvYDOsA',
+    original:
+    'https://cdn.pixabay.com/photo/2020/09/05/05/45/squirrel-5545638_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://phoneky.co.uk/thumbs/screensavers/down/misc/circles320_qr4vhh1d.gif',
+    original:
+    'https://cdn.pixabay.com/photo/2020/09/07/09/33/grapes-5551325_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
---
+++ gallery.js
@@ -0,0 +1,62 @@
+import gallerySrc from "./gallery-items.js";
+
+const galleryUl = document.querySelector(".js-gallery");
+const btnRef = document.querySelector("button[data-action=close-lightbox]");
+const openModal = document.querySelector(".lightbox");
+const modalImgRef = document.querySelector(".lightbox__image");
+
+const divContentRef = document.querySelector(".lightbox__content");
+
+galleryUl.addEventListener("click", onOpenModal);
+btnRef.addEventListener("click", onCloseModal);
+
+divContentRef.addEventListener("click", divOffModal);
+
+const createGallery = (images) => {
+  const galleryLi = document.createElement("li");
+  galleryLi.classList.add("gallery__item");
+  const galleryLinc = document.createElement("a");
+  galleryLinc.classList.add("gallery__linc");
+  galleryLinc.setAttribute("href", "#");
+  const galleryImg = document.createElement("img");
+  galleryImg.classList.add("gallery__image");
+  galleryImg.setAttribute("src", images.preview);
+  galleryImg.setAttribute("alt", images.description);
+  galleryImg.setAttribute("data-source", images.original);
+  galleryLinc.appendChild(galleryImg);
+  galleryLi.appendChild(galleryLinc);
+
+  return galleryLi;
+};
+
+const container = gallerySrc.map((image) => createGallery(image));
+
+galleryUl.append(...container);
+
+function onOpenModal(event) {
+  window.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      console.log("object");
+      onCloseModal();
+    }
+  });
+  if (event.target.nodeName !== "IMG") {
+    return console.log("object");
+  }
+  const dataUrlImg = event.target.dataset.source;
+  console.log(dataUrlImg);
+
+  openModal.classList.add("is-open");
+  modalImgRef.src = dataUrlImg;
+}
+
+function onCloseModal() {
+  openModal.classList.remove("is-open");
+  modalImgRef.src = "";
+}
+
+function divOffModal(event) {
+  if (event.target === event.currentTarget) {
+    onCloseModal();
+  }
+}
